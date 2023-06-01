f6f39320a9a9633bb356ab3d80fdccc95e045ce2
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-06-01 14:54:01 +0600

    Cart item delete api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion , ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
@@ -16,7 +17,6 @@ app.listen(port , () => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ndtaujs.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -60,6 +60,13 @@ async function run() {
         }
         const query = { email : email }
         const result = await cartCollection.find(query).toArray()
+        res.send(result)
+    })
+
+    app.delete('/carts/:id' , async(req , res) => {
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id) }
+        const result = await cartCollection.deleteOne(query)
         res.send(result)
     })
 
