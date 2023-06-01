2ea7c9bb4f0c6c613a7a2f5061c6bed8cb9d4142
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-06-01 14:51:59 +0600

    tanstack query api

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ async function run() {
 
     const menuCollection = client.db('bistroDb').collection('menu')
     const reviewCollection = client.db('bistroDb').collection('reviews')
+    const cartCollection = client.db('bistroDb').collection('carts')
 
     app.get('/menu' , async(req , res) => {
         const result = await menuCollection.find().toArray()
@@ -41,6 +42,24 @@ async function run() {
 
     app.get('/reviews' , async(req , res) => {
         const result = await reviewCollection.find().toArray()
+        res.send(result)
+    })
+
+    // Cart
+    app.post('/carts' , async(req , res) => {
+        const item = req.body;
+        console.log(item)
+        const result = await cartCollection.insertOne(item)
+        res.send(result)
+    })
+
+    app.get('/carts' , async(req , res) => {
+        const email = req.query.email;
+        if(!email) {
+            res.send([]);
+        }
+        const query = { email : email }
+        const result = await cartCollection.find(query).toArray()
         res.send(result)
     })
 
